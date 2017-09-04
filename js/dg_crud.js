c2d4b09fffa690fcae913c26447a567313b3adbf
Walter Cardelli (imac) <waltex@libero.it>
2017-09-04 22:40:44 +0200

    easyui gii initial commit

--- js/dg_crud.js
+++ js/dg_crud.js
@@ -1,5 +1,43 @@
-
+var g_debug
+var g_keydown
 function init_app() {
+    $('#tb_name_app').textbox({
+        required: true,
+        label: T("Nome app:"), //path app
+        prompt: T("digita qui..."), //type here
+        labelPosition: 'top',
+        onChange: function (newValue, oldValue) {
+            //var val = $('#tb_out').textbox('getValue') + newValue;
+            //$('#tb_out').textbox('setValue', val);
+        }
+    });
+    /*
+    $('#tb_name_app').textbox('textbox').bind('keydown', function (e) {
+        g_debug = e;
+        if ((e.keyCode >= 32) & (e.keyCode <= 126)) {
+            var val = $('#tb_out').textbox('getValue') + e.key;
+            $('#tb_out').textbox('setValue', val);
+        }
+    });
+    */
+
+    $('#tb_name_app').textbox('textbox').bind('keydown', function (e) {
+        var $this = $(this);
+        window.setTimeout(function () {
+            g_debug = e;
+            console.log($this.val() + '--' + $this.val().length);
+            var add = $this.val();
+            if (add.length == 1) {
+                var val = $('#tb_out').textbox('getValue');
+                g_keydown = val
+                add = val + add;
+            } else {
+                add = g_keydown + add;
+            }
+            $('#tb_out').textbox('setValue', add);
+        }, 0);
+    });
+
     $('#tb_out').textbox({
         value: g_param["percorso del codice generato"],
         required: true,
@@ -16,9 +54,11 @@ function init_app() {
     $('#p_crud').panel({
         title: T("Altri Parametri"),
     });
+
+    $('#bt_runcode').linkbutton({text: T('Esegui')});
     // expand and collapse on click
     $('#p_crud').panel('header').click(function () {
         var hide = $($('#p_crud').panel('body')[0]).css('display') == 'none';
         (hide) ? $('#p_crud').panel('expand', true) : $('#p_crud').panel('collapse', true);
-    })
+    });
 }
