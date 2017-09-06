865152d6dc576af9f1f31701c89e69004f7d126f
walter (abb) <waltex@libero.it>
2017-09-06 13:00:53 +0200

    initial release

---
+++ lib/easyui_gii/class.crud.php
@@ -0,0 +1,25 @@
+<?php
+
+class crud {
+
+    // proprietà
+    //public $a = 10;
+
+    /**
+     * @param string $name_app name application
+     * @param string $folder_app path outuput code generated
+     * @param string $table_name name table
+     * @param array $opt additional option
+     */
+    function __construct($app_name, $app_folder, $table_name, $opt) {
+        $this->app_name = $app_name;
+        $this->app_folder = $app_folder;
+        $this->table_name = $table_name;
+    }
+
+    // metodi
+    public function render() {
+        echo "test class ";
+    }
+
+}
