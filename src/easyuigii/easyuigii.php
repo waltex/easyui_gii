dbeb7727e25db7a4875c12e5f51488eb321b5c6f
walter (abb) <waltex@libero.it>
2017-09-11 17:53:59 +0200

    initial release

--- src/easyuigii/easyuigii.php
+++ src/easyuigii/easyuigii.php
@@ -18,8 +18,57 @@ class easyuigii {
         $this->table_name = $table_name;
     }
 
+    /** folder create and file
+     */
+    public function buildAppCrud() {
+        //folder create
+        $dir = $_SERVER['DOCUMENT_ROOT'] . "/" . $this->app_folder;
+        if (!is_dir($dir)) {
+            mkdir($dir, 0777, true); //create folder
+        } else {
+            $this->rrmdir($dir); //empty
+            mkdir($dir, 0777, true); //create folder and folder below
+        }
+        //build template
+        
+        $loader = new \Twig_Loader_Filesystem('../src/template');
+        $twig = new \Twig_Environment($loader);
+        $html = $twig->render('crud.html', array('test' => 'Fabien'));
+        $file = $dir . "/index.htm";
+        file_put_contents($file, $html); //write generated html
+
+        $zip = new \ZipArchive;
+        //$zip_file = __DIR__ . "/src/template/base/lib.zip";
+        $zip_file = \dirname(__FILE__) . 'easyui_gii/src/template/base/lib.zip';
+        $file = $zip->open($zip_file);
+        $zip->extractTo($dir . "/");
+        $zip->close();
+    }
+
+
+
+    /**
+     * remove empty dir
+     * @param string $dir directory
+     */
+    private function rrmdir($dir) {
+        if (is_dir($dir)) {
+            $objects = scandir($dir);
+            foreach ($objects as $object) {
+                if ($object != "." && $object != "..") {
+                    if (filetype($dir . "/" . $object) == "dir")
+                        rrmdir($dir . "/" . $object);
+                    else
+                        unlink($dir . "/" . $object);
+                }
+            }
+            reset($objects);
+            rmdir($dir);
+        }
+    }
+
     // metodi
-    public function render() {
+    public function test() {
         echo "test class ";
     }
 
