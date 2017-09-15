4bcca077148f4ff414944fd2a25e45bd2af1d121
walter (abb) <waltex@libero.it>
2017-09-15 12:04:50 +0200

    initial release

---
+++ src/template/base/api/api_1_declare.php
@@ -0,0 +1,21 @@
+<?php
+
+if (function_exists('xdebug_disable')) {
+    //xdebug_disable();
+}
+
+require '../vendor/autoload.php';
+
+$app = new Slim\Slim();
+
+//Add the middleware globally
+$app->add(new \SlimJson\Middleware(array(
+    'json.status' => false,
+    'json.override_error' => true,
+    'json.override_notfound' => true,
+    'json.debug' => false,
+    'json.cors' => true
+)));
+
+//$app->get('/test_api', 'test_api'); // test api
+
