19495d9d059f2fbd0e453a1377bec2b52e2cf030
walter (abb) <waltex@libero.it>
2017-10-06 11:02:20 +0200

    initial release

--- js/index.js
+++ js/index.js
@@ -7,26 +7,26 @@ function init_app() {
 
 
 
-    addTab('welcome', 'welcome.html', null);
+    addTab('welcome', 'welcome.html', {iconCls: 'fa fa-user-circle-o fa-lg'});
 
     var menu = '<a id="bt_menu">' + T("Menu") + '</a>'; //Menu
     $('#layout_main').layout('panel', 'center').panel({title: 'Easyui Gii - Code Generetor 1.0   ' + menu})
 
 
      $('#bt_menu').menubutton({
-        iconCls: 'icon-edit',
+        iconCls: 'fa fa-bars fa-lg',
         menu: '#mm_app',
         height: '15px',
     });
 
     $('#dg_crud').on('click', function () {
-        addTab('crud', 'dg_crud.html', null);
+        addTab('crud', 'dg_crud.html', {iconCls: 'fa fa-table fa-lg fa-brown'});
     });
     $('#dg_set').on('click', function () {
-        addTab(T('impostazioni'), 'settings.html', null);
+        addTab(T('impostazioni'), 'settings.html', {iconCls: 'fa fa fa-cog fa-lg fa-grey'});
     });
     $('#dg_snippets').on('click', function () {
-        addTab('snippets', 'snippets.html', null);
+        addTab('snippets', 'snippets.html', {iconCls: 'fa fa-file-code-o fa-lg fa-blue'});
     });
 }
 function addTab(title, url, opt) {
@@ -43,17 +43,35 @@ function addTab(title, url, opt) {
                     title_open = title + "_" + i;
                     (!$('#tab_app').tabs('exists', title_open)) ? new_title = true : false;
                 }
-                addTab_sub(T(title_open), url, opt);//new tab clone
+                //addTab_sub(T(title_open), url, opt);//new tab clone
+                var iconCls = opt_iconCls(opt);
+                addTab_sub(add_icon_title(T(title_open), iconCls), url, opt);//new tab clone
             } else {
                 $('#tab_app').tabs('select', title_open);
             }
         });
     } else {
-        addTab_sub(T(title_open), url, opt);// new tab
+        //addTab_sub(T(title_open), url, opt);// new tab
+        var iconCls = opt_iconCls(opt);
+        addTab_sub(add_icon_title(T(title_open), iconCls), url, opt);// new tab
     }
 }
+
+function add_icon_title(title, iconCls) {
+    if (iconCls != "") {
+        return '<span class="fa ' + iconCls + '"><span style="font: 80% sans-serif;margin-left:2px;color:black">' + title + '</span></span>';
+    } else {
+        return title;
+    }
+}
+
 function addTab_sub(title, url, opt) {
-    var content = '<iframe id="iframe_' + title + '" scrolling="yes" frameborder="0"  src="' + url + '" style="width:99%;height:97.3%;padding:0.5%"></iframe>';
+    if ($(title)[0] !== undefined) {
+        var title_frame = $(title)[0].innerText;
+    } else {
+        var title_frame = title;
+    }
+    var content = '<iframe id="iframe_' + title_frame + '" scrolling="yes" frameborder="0"  src="' + url + '" style="width:99%;height:97.3%;padding:0.5%"></iframe>';
     var tab = $('#tab_app').tabs('add', {
         title: title,
         content: content,
@@ -63,8 +81,28 @@ function addTab_sub(title, url, opt) {
         tools: [{
                 iconCls: 'icon-mini-refresh',
                 handler: function () {
-                    document.getElementById('iframe_' + title).contentDocument.location.reload(true);
+                    var title_name = get_title(title);
+                    document.getElementById('iframe_' + title_name).contentDocument.location.reload(true);
                 }
             }]
     });
 }
+// get title and remove span icon
+function get_title(title) {
+    if ($(title)[0] !== undefined) {
+        return  $(title)[0].innerText;
+    } else {
+        return title;
+    }
+}
+
+function opt_iconCls(opt) {
+    if (opt != null) {
+        if (opt.hasOwnProperty('iconCls')) {
+            return opt.iconCls;
+        }
+    } else {
+        return '';
+    }
+}
+
