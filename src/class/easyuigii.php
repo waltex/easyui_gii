a356fc3e6f203eefcc519cb8e1a730e87c09ccdf
Walter Cardelli (imac) <waltex@libero.it>
2017-10-22 22:21:46 +0200

    crud inline

--- src/class/easyuigii.php
+++ src/class/easyuigii.php
@@ -843,9 +843,9 @@ class easyuigii {
         if ($row["CK"] == "1") {
             $yes = $this->T("si");
             $no = $this->T("no");
-            $data = "[{text: '$yes', value:1}, {text:'$no', value:0}]";
-            $editor = "$id_object" . "combobox({" . PHP_EOL . " $width $label $required panelHeight:50,data:$data,});" . PHP_EOL;
-            $editor = str_replace(",", "," . PHP_EOL, $editor);
+            $data = "[{text: '$yes',value:1},{text:'$no',value:0}]";
+            $editor = "$id_object" . "combobox({" . PHP_EOL . " $width $label $required panelHeight:50, data:$data,});" . PHP_EOL;
+            $editor = str_replace(", ", "," . PHP_EOL, $editor); //only space return dot
             return $editor;
         }
 
